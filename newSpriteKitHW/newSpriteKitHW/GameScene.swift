cce79881e3150f53229aaf343b605c8491d6a728
Anastasiia <101048488+AAleksandrovna@users.noreply.github.com>
2023-01-04 13:30:12 +0100

    almost finish

--- newSpriteKitHW/newSpriteKitHW/GameScene.swift
+++ newSpriteKitHW/newSpriteKitHW/GameScene.swift
@@ -10,123 +10,89 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let round = SKShapeNode(circleOfRadius: 45)
+    let round = SKShapeNode(circleOfRadius: 30)
+    var slider = UISlider(frame: CGRect(x: 0, y: 0, width: 150, height: 20))
+    
     
     override func didMove(to view: SKView) {
-        backgroundColor = randomColor()
         
         round.fillColor = .black
         round.position.x = frame.midX
         round.position.y = frame.midY
         round.physicsBody = .init(rectangleOf: round.frame.size)
         round.physicsBody?.isDynamic = false
+        round.speed = num
         self.addChild(round)
+        
+        slider.tintColor = UIColor.black
+        slider.maximumTrackTintColor = UIColor.white
+        
         view.addSubview(slider)
-    
+        
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            slider.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.7),
+            slider.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            slider.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+        ])
+        
+        loadSlider()
+        self.changeColor()
     }
     
-    func randomColor() -> UIColor {
-        return UIColor(red: .random(in: 0.1...1),
-                       green: .random(in: 0.1...1),
-                       blue: .random(in: 0.1...1),
-                       alpha: 1.0)
+    func changeColor() {
+        let runAction = SKAction.run { self.randomColor() }
+        self.run(.repeatForever(runAction), withKey: "changeBgKey")
+    }
+    func randomColor() {
+        self.backgroundColor = .init(
+            red: .random(in: 0.1...1),
+            green: .random(in: 0.1...1),
+            blue: .random(in: 0.1...1),
+            alpha: 1.0)
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let position = touches.first!.location(in: self)
-        if let node = self.nodes(at: position).first {
-            node.physicsBody = nil
+    var isMovingBack = true
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let location = touches.first?.location(in: self) else { return }
+        let node = self.atPoint(location)
+        
+        if node == self {
+            let action = self.action(forKey: "changeBgKey")
+            action?.speed = self.num
+            
         }
+        
+        node.run(goToCorner)
         
     }
     
-    let slider = UISlider(frame: CGRect(x: 250, y: 250, width: 280, height: 20))
+    var num: CGFloat = 1
+    var goToCorner: SKAction {
+        let spacer = round.frame.width * 0.5
+        let moveToUpperCorner = SKAction.move(to: .init(x: (frame.maxX - spacer),
+                                                        y: (frame.minY + spacer)),
+                                              duration: TimeInterval(num))
+        
+        let moveToLowerCorner = SKAction.move(to: .init(x: (frame.minX + spacer),
+                                                        y: (frame.maxY - spacer)),
+                                              duration: TimeInterval(num))
+        
+        let sqnc = SKAction.sequence([moveToLowerCorner, moveToUpperCorner])
+        round.run(SKAction.repeatForever(sqnc), withKey: "key")
+        
+        return sqnc
+    }
     
+    private func loadSlider() {
+        slider.minimumValue = 1
+        slider.maximumValue = 4
+        view?.addSubview(slider)
+        slider.addTarget(self, action: #selector(sliderMoved(_:)), for: .valueChanged)
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //    private var label : SKLabelNode?
-    //    private var spinnyNode : SKShapeNode?
-    //
-    //    override func didMove(to view: SKView) {
-    //
-    //        // Get label node from scene and store it for use later
-    //        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-    //        if let label = self.label {
-    //            label.alpha = 0.0
-    //            label.run(SKAction.fadeIn(withDuration: 2.0))
-    //        }
-    //
-    //        // Create shape node to use during mouse interaction
-    //        let w = (self.size.width + self.size.height) * 0.05
-    //        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-    //
-    //        if let spinnyNode = self.spinnyNode {
-    //            spinnyNode.lineWidth = 2.5
-    //
-    //            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-    //            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-    //                                              SKAction.fadeOut(withDuration: 0.5),
-    //                                              SKAction.removeFromParent()]))
-    //        }
-    //    }
-    //
-    //
-    //    func touchDown(atPoint pos : CGPoint) {
-    //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-    //            n.position = pos
-    //            n.strokeColor = SKColor.green
-    //            self.addChild(n)
-    //        }
-    //    }
-    //
-    //    func touchMoved(toPoint pos : CGPoint) {
-    //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-    //            n.position = pos
-    //            n.strokeColor = SKColor.blue
-    //            self.addChild(n)
-    //        }
-    //    }
-    //
-    //    func touchUp(atPoint pos : CGPoint) {
-    //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-    //            n.position = pos
-    //            n.strokeColor = SKColor.red
-    //            self.addChild(n)
-    //        }
-    //    }
-    //
-    //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        if let label = self.label {
-    //            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-    //        }
-    //
-    //        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    //    }
-    //
-    //    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    //    }
-    //
-    //    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    //    }
-    //
-    //    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    //    }
-    //
-    //
+    @objc func sliderMoved(_ sender: UISlider) {
+        num = CGFloat(slider.value)
+    }
     
 }
--- newSpriteKitHW/newSpriteKitHW/GameViewController.swift
+++ newSpriteKitHW/newSpriteKitHW/GameViewController.swift
@@ -21,37 +21,17 @@ class GameViewController: UIViewController {
     private func setupGameVC () {
         
         if let skView = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                //                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-
-                // Present the scene
-                skView.presentScene(scene)
-                //           }
-                
-                skView.ignoresSiblingOrder = true
-                
-                skView.showsFPS = true
-                skView.showsNodeCount = true
-            }
+            let scene = GameScene(size: view.bounds.size)
+            
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .resizeFill
+            
+            // Present the scene
+            skView.presentScene(scene)
+            
+            skView.ignoresSiblingOrder = true
+            skView.showsFPS = true
+            skView.showsNodeCount = true
         }
     }
-    
-    //
-    //    override var shouldAutorotate: Bool {
-    //        return true
-    //    }
-    //
-    //    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-    //        if UIDevice.current.userInterfaceIdiom == .phone {
-    //            return .allButUpsideDown
-    //        } else {
-    //            return .all
-    //        }
-    //    }
-    //
-    //    override var prefersStatusBarHidden: Bool {
-    //        return true
-    //    }
 }
